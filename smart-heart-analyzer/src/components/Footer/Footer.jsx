581403fa40df1eff4adb8310eaadb498873fa950
dinhhieu1208 <nguyenhieu96hth@gmail.com>
2025-10-25 22:53:34 +0700

    [Feature] Trang footer

---
+++ smart-heart-analyzer/src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-red-800 text-center p-4 mt-8 text-white">
+      <p>© 2024 Smart Heart Analyzer. All rights reserved.</p>
+    </footer>
+  );
+};
