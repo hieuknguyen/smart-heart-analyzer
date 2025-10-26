05b4182950a4553f0100fff1e7c52c72aa6a5ef5
dinhhieu1208 <nguyenhieu96hth@gmail.com>
2025-10-26 21:54:28 +0700

    [Feature] Trang Header and Footer

---
+++ smart-heart-analyzer/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+export const LayoutClient = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
