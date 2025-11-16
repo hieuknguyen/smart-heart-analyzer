6f7591c03209b6241573802ae27469ef9bc64d9a
hieuknguyen <nguyentrunghieu4390@gmail.com>
2025-11-16 21:57:30 +0700

    add middleware

--- backend/user-service/app/main.py
+++ backend/user-service/app/main.py
@@ -1,11 +1,18 @@
 #luon doc AI_GUIDELINES.md
-from fastapi import FastAPI
+from fastapi import FastAPI,
 from app.routers.users import router as users
 from app.database.database import create_tables, initialize_database, _pool
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def on_startup():
